8527eac78ca6d1dafc2c3835870c434662a49ddb
LynKiss <lyn25092004@gmail.com>
2026-01-06 01:46:16 +0700

    chi tiết sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -176,3 +176,21 @@ module.exports.editPatch = async (req, res) => {
   }
   res.redirect(`${systemConfig.prefixAdmin}/products`);
 };
+// [GET] /admin/products/edit/:id
+module.exports.detail = async (req, res) => {
+  try {
+    const find = {
+      deleted: false,
+      _id: req.params.id,
+    };
+    const product = await Product.findOne(find); // find thì là trả về nhiều bản ghi findOne là 1
+    console.log(product);
+    res.render("admin/pages/products/detail", {
+      pageTitle: product.title,
+      product: product,
+    });
+  } catch (error) {
+    res.redirect(` ${systemConfig.prefixAdmin}/products`);
+    req.flash("error", `Không tồn tại sản phẩm này !`);
+  }
+};
