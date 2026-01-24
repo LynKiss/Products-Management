1465a55fbf97a6849a4671c8d5eb96af6d06004c
LynKiss <lyn25092004@gmail.com>
2026-01-24 17:04:48 +0700

    fix : Danh mục sản phẩn , design client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,8 @@
 const productRoutes = require("./products.route");
 const homeRoutes = require("./home.route");
+const categoryMiddleWare = require("../../middlewares/client/category.middleware");
 module.exports = (app) => {
+  app.use(categoryMiddleWare.category); // dùng cho toàn bộ trang web
   app.use("/", homeRoutes);
   app.use("/products", productRoutes);
 };
