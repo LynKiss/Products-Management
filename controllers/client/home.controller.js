118558953d884a6ba6a79f563453d951e72695a3
LynKiss <lyn25092004@gmail.com>
2026-01-26 01:34:10 +0700

    refactor : Hiển thị 1 vài sản phẩm mới trang home

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,15 +2,24 @@ const productsHelper = require("../../helpers/products");
 const Product = require("../../models/product.model");
 // [GET] /
 module.exports.index = async (req, res) => {
-  const productsFeatured = await Product.find({
+  const newProductsFeatured = await Product.find({
     featured: "1",
     deleted: false,
     status: "active",
   }).limit(3);
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  const newProducts = productsHelper.priceNewProducts(newProductsFeatured);
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active",
+  })
+    .sort({ position: "desc" })
+    .limit(3);
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  // Đổi giảm giá tiền nếu có lấy theo positon tại vì bình thường có thể thay đổi vị trí
 
   res.render("client/pages/home/index.pug", {
     pageTitle: " Trang chủ ",
     productsFeatured: newProducts,
+    productsNew: newProductsNew,
   });
 };
