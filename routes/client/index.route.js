248d24337276f08f9f6658986f3a70ca85f1608b
LynKiss <lyn25092004@gmail.com>
2026-01-26 13:21:19 +0700

    feat : Tính năng tìm kiếm bằng keyword bên client

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,10 @@
 const productRoutes = require("./products.route");
 const homeRoutes = require("./home.route");
+const searchRoutes = require("./search.route");
 const categoryMiddleWare = require("../../middlewares/client/category.middleware");
 module.exports = (app) => {
   app.use(categoryMiddleWare.category); // dùng cho toàn bộ trang web
   app.use("/", homeRoutes);
+  app.use("/search", searchRoutes);
   app.use("/products", productRoutes);
 };
