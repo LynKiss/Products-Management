88355e937afe5bbc109de7a1e3f8859c71cbc636
LynKiss <lyn25092004@gmail.com>
2026-01-28 12:56:36 +0700

    feat : Đăng ký tài khoản user

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -3,6 +3,7 @@ const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
 const checkoutRoutes = require("./checkout.route");
+const userRoutes = require("./user.route");
 const categoryMiddleWare = require("../../middlewares/client/category.middleware");
 const cartMiddleWare = require("../../middlewares/client/cart.middleware");
 module.exports = (app) => {
@@ -13,4 +14,5 @@ module.exports = (app) => {
   app.use("/search", searchRoutes);
   app.use("/products", productRoutes);
   app.use("/checkout", checkoutRoutes);
+  app.use("/user", userRoutes);
 };
