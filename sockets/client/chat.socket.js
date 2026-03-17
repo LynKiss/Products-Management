3c99e1b15203b9438f8b1c3373042213fbb67bbd
Lyn <lyn250904@gmail.com>
2026-03-18 01:50:59 +0700

    refactor : tối ưu chat socketIO

---
+++ sockets/client/chat.socket.js
@@ -0,0 +1,42 @@
+const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
+const Chat = require("../../models/chat.model");
+module.exports = (res) => {
+    const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
+    _io.once("connection", (socket) => {
+        socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+            let images = [];
+            for (const imageBuffer of data.images) {
+                const link = await uploadToCloudinary(imageBuffer);
+                images.push(link)
+
+            }
+            const chat = new Chat({
+                user_id: userId,
+                content: data.content,
+                images: images
+            });
+
+            await chat.save();
+            //Trả về client
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: data.content,
+                images: images
+            })
+        });
+        // Typing
+        socket.on("CLIENT_SEND_TYPING", async (type) => {
+            socket.broadcast.emit("SERVER_SEND_TYPING", {
+                userId: userId,
+                fullName: fullName,
+                type: type
+            })
+
+        });
+        // End Typing
+    });
+
+}
