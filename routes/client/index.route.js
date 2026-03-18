381b34e1198bde13b07986c03fe23d4e94bb7b15
Lyn <lyn250904@gmail.com>
2026-03-18 11:09:53 +0700

    Phân tích chức năng chat , danh sach bạn bè

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -10,6 +10,7 @@ const cartMiddleWare = require("../../middlewares/client/cart.middleware");
 const userMiddleWare = require("../../middlewares/client/user.middleware");
 const settingMiddleWare = require("../../middlewares/client/setting.middleware");
 const authMiddleWare = require("../../middlewares/client/requireAuth.middleware");
+const usersRoutes = require("./users.route")
 module.exports = (app) => {
   app.use(categoryMiddleWare.category); // dùng cho toàn bộ trang web
   app.use(cartMiddleWare.cartId);
@@ -22,4 +23,5 @@ module.exports = (app) => {
   app.use("/checkout", checkoutRoutes);
   app.use("/user", userRoutes);
   app.use("/chat", authMiddleWare.requireAuth, chatRoutes);
+  app.use("/users", authMiddleWare.requireAuth, usersRoutes);
 };
