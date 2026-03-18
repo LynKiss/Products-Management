5e307a6d3bcf66320cbe7213039b97c45b1893fd
Lyn <lyn250904@gmail.com>
2026-03-18 23:45:44 +0700

    feat : logic page add friend , acceptFriend

--- controllers/client/users.controller.js
+++ controllers/client/users.controller.js
@@ -30,8 +30,25 @@ module.exports.notFriend = async (req, res) => {
 };
 
 // [GET] /user/request
-module.exports.register = async (req, res) => {
-  res.render("client/pages/user/request", {
-    pageTitle: "Lời mời đã gửi",
-  });
+module.exports.request = async (req, res) => {
+    // SOCKET
+    usersSocket(res);
+    // END SOCKET
+    const userId = res.locals.user.id;
+
+    const myUser = await User.findOne({
+        _id: userId
+    });
+    const requestFriends = myUser.requestFriends;
+    const acceptFriends = myUser.acceptFriends;
+    const users = await User.find({
+        _id: { $in: requestFriends },
+        status: "active",
+        deleted: false
+    }).select("id avatar fullName");
+
+    res.render("client/pages/users/request", {
+        pageTitle: "Lời mời đã gửi",
+        users: users
+    });
 };
--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -1,6 +1,7 @@
 const User = require("../../models/user.model")
 module.exports = (res) => {
     _io.once("connection", (socket) => {
+        // Chức năng gửi yêu cầu 
         socket.on("CLIENT_ADD_FRIEND", async (userId) => {
             const myUserId = res.locals.user.id;
 
@@ -21,7 +22,7 @@ module.exports = (res) => {
             // Thêm id của B vào requestFriend của A
             const exitsIdBinA = await User.findOne({
                 _id: myUserId,
-                requestFriends: myUserId
+                requestFriends: userId
             })
             if (!exitsIdBinA) {
                 await User.updateOne(
@@ -30,5 +31,38 @@ module.exports = (res) => {
                 )
             }
         });
+        // END Chức năng gửi yêu cầu 
+
+        // Chức năng hủy gửi yêu cầu
+        socket.on("CLIENT_CANCEL_FRIEND", async (userId) => {
+            const myUserId = res.locals.user.id;
+
+            // Xóa id của A vào acceptFriend của B
+            const exitsIdAinB = await User.findOne({
+                _id: userId,
+                acceptFriends: myUserId
+            })
+
+            if (exitsIdAinB) {
+                await User.updateOne({
+                    _id: userId
+                }, {
+                    $pull: { acceptFriends: myUserId }
+                }
+                );
+            }
+            // Xóa id của B vào requestFriend của A
+            const exitsIdBinA = await User.findOne({
+                _id: myUserId,
+                requestFriends: userId
+            })
+            if (exitsIdBinA) {
+                await User.updateOne(
+                    { _id: myUserId },
+                    { $pull: { requestFriends: userId } }
+                )
+            }
+        });
+        // END Chức năng hủy gửi yêu cầu
     });
 }
